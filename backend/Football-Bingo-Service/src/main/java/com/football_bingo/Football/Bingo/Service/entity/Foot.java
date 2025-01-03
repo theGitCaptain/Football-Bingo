90126f7675905e865b52261dc1b165b7eda0956b
Julian Dahl <juliandahl99@gmail.com>
2025-01-03 14:55:32 +0100

    Foot entity

--- backend/Football-Bingo-Service/src/main/java/com/football_bingo/Football/Bingo/Service/entity/Foot.java
+++ backend/Football-Bingo-Service/src/main/java/com/football_bingo/Football/Bingo/Service/entity/Foot.java
@@ -1,10 +1,14 @@
 package com.football_bingo.Football.Bingo.Service.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "feet")
