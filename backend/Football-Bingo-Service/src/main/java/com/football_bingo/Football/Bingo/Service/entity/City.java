15cf7be826e7a2f99b65e9bba249f9a61af2d19e
Julian Dahl <juliandahl99@gmail.com>
2025-01-03 14:55:22 +0100

    City Entity

--- backend/Football-Bingo-Service/src/main/java/com/football_bingo/Football/Bingo/Service/entity/City.java
+++ backend/Football-Bingo-Service/src/main/java/com/football_bingo/Football/Bingo/Service/entity/City.java
@@ -1,11 +1,17 @@
 package com.football_bingo.Football.Bingo.Service.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "cities")
 public class City {
