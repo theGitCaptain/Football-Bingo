6d288e88efde7ec78aba955336b16ab6cd62cb12
Julian Dahl <juliandahl99@gmail.com>
2025-01-03 14:56:01 +0100

    Updated Player entity

--- backend/Football-Bingo-Service/src/main/java/com/football_bingo/Football/Bingo/Service/entity/Player.java
+++ backend/Football-Bingo-Service/src/main/java/com/football_bingo/Football/Bingo/Service/entity/Player.java
@@ -1,11 +1,16 @@
 package com.football_bingo.Football.Bingo.Service.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "players")
@@ -44,4 +49,52 @@ public class Player {
     private Boolean enabled;
 
     private LocalDateTime lastIterated;
+
+    @ManyToMany
+    @JoinTable(
+            name = "players_clubs",
+            joinColumns = @JoinColumn(name = "player_id"),
+            inverseJoinColumns = @JoinColumn(name = "club_id")
+    )
+    private List<Club> clubs;
+
+    @ManyToMany
+    @JoinTable(
+            name = "players_nations",
+            joinColumns = @JoinColumn(name = "player_id"),
+            inverseJoinColumns = @JoinColumn(name = "nation_id")
+    )
+    private List<Nation> citizenships;
+
+    @ManyToMany
+    @JoinTable(
+            name = "players_awards",
+            joinColumns = @JoinColumn(name = "player_id"),
+            inverseJoinColumns = @JoinColumn(name = "achievement_id")
+    )
+    private List<Achievement> achievements;
+
+    @ManyToMany
+    @JoinTable(
+            name = "players_coaches",
+            joinColumns = @JoinColumn(name = "player_id"),
+            inverseJoinColumns = @JoinColumn(name = "coach_id")
+    )
+    private List<Coach> coaches;
+
+    @ManyToMany
+    @JoinTable(
+            name = "teammates",
+            joinColumns = @JoinColumn(name = "player1_id"),
+            inverseJoinColumns = @JoinColumn(name = "player2_id")
+    )
+    private List<Player> directTeammates;
+
+    @ManyToMany
+    @JoinTable(
+            name = "teammates",
+            joinColumns = @JoinColumn(name = "player2_id"),
+            inverseJoinColumns = @JoinColumn(name = "player1_id")
+    )
+    private List<Player> inverseTeammates;
 }
